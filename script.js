96d1a645f2657ffa4ea0396b37cd691f73bcf979
Siyam Ahmed <siyam.eng.du@gmail.com>
2025-06-10 23:19:42 +0600

    add '/' to the relative link

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
 // Load the data
 const cardsContainer = document.getElementById("cardsContainer");
-const timeTrackerPromise = fetch("time-tracking-dashboard/data.json"); // for Github Pages
+const timeTrackerPromise = fetch("/time-tracking-dashboard/data.json"); // for Github Pages
 // const timeTrackerPromise = fetch("/data.json"); // for Local server
 const primaryColors = [
   "--Orange-300",
